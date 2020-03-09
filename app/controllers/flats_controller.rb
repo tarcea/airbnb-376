e37d34738801006681aeffcfa9cc1f9138d3093a
valerie schraauwers <valerieschraauwers@gmail.com>
2020-03-09 17:53:16 +0100

    Add system test and implement controller actions

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,21 @@
+require 'open-uri'
+class FlatsController < ApplicationController
+  before_action :set_flats, only: [:index, :show]
+
+  def index
+  end
+
+  def show
+    @flat = @flats.find do |flat|
+      flat['id'] == params[:id].to_i
+    end
+  end
+
+  private
+
+  def set_flats
+    url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
+    @flats = JSON.parse(open(url).read)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+get 'flats', to:'flats#index'
+get 'flats/:id', to: 'flats#show', as: :flat
 end
